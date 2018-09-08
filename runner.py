788d431ced9cb0bf5bed0cd3a6882e1b18cfcf9b
Mikkel Jakobsen <mikkel.aunsbjerg@prevas.dk>
2018-09-08 11:32:51 +0200

    improved runner script

--- runner.py
+++ runner.py
@@ -1,4 +1,4 @@
-import os
+import shutil
 import sys
 import subprocess
 import time
@@ -7,14 +7,14 @@ if __name__ == '__main__':
     exitcode = 0
 
     try:
-        p1 = subprocess.Popen(['pipenv', 'run', 'python', 'robotdebug/Listener.py', 'server', 'localhost', '5555'])
+        p1 = subprocess.Popen(['pipenv', 'run', 'python', 'robotdebug/DebugServer.py', 'localhost', '5555'])
 
-        time.sleep(1)
-        p2 = subprocess.Popen(['pipenv', 'run', 'robot', '--listener', 'Listener:localhost:5555', 
-            '--pythonpath', 'robotdebug', '--outputdir', 'results', 'robotdebug/demo.robot'], stdout=subprocess.PIPE)
-        
+        time.sleep(2)
+        p2 = subprocess.Popen(['pipenv', 'run', 'robot', '--listener', 'DebugListener:localhost:5555',
+            '--pythonpath', 'robotdebug', '--outputdir', 'results', 'tests/robot/demo.robot'])
+
         p2.communicate()
-        os.rmdir('results')
+        shutil.rmtree('results')
 
     except Exception as exc:
         print(exc)
